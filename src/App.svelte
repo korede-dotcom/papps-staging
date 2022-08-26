bcb254fa5cbd07734e2c7a0df5c8150198c546d1
koredebada <koredebada@gmail.com>
2022-08-26 15:28:09 +0100

    papps staging

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,6 @@
 <script>
   import svelteLogo from './assets/svelte.svg'
   import Counter from './lib/Counter.svelte'
-  // import Img from './assets/Background.png'
   import { Router, Link, Route } from "svelte-routing";
 
   import Login from './components/Login.svelte';
@@ -15,7 +14,7 @@
   import {navigate} from 'svelte-routing';
  import { onMount } from 'svelte';
 
- ./assets/background.png
+
 
   export let url = "";
  
